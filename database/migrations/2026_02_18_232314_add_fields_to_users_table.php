1c3895cf0ca2cede4cedf4d9ca62697efafb46c4
Sani <sani_ragimov@mail.ru>
2026-02-19 04:06:11 +0300

    Add User, Task, TaskResponse migrations

---
+++ database/migrations/2026_02_18_232314_add_fields_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('client')->index()->after('email');
+            $table->string('avatar')->nullable()->after('role');
+            $table->boolean('is_blocked')->default(false)->after('avatar');
+            $table->decimal('balance', 12, 2)->default(0)->after('is_blocked');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex(['role']);
+            $table->dropColumn(['role', 'avatar', 'is_blocked', 'balance']);
+        });
+    }
+};
